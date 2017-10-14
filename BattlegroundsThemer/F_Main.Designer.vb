21eee1076fe8d7b7e8a55deb59c7fd6c2df12726
FreddieDev <freddiechessell83@gmail.com>
2017-10-14 14:42:12 +0100

    Added WIP resourses and new forms

---
+++ BattlegroundsThemer/F_Main.Designer.vb
@@ -0,0 +1,412 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class F_Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.TB_SpoofedUsername = New System.Windows.Forms.TextBox()
+        Me.RB_UsernameSpoof = New System.Windows.Forms.RadioButton()
+        Me.RB_UsernameHide = New System.Windows.Forms.RadioButton()
+        Me.RB_UsernameShow = New System.Windows.Forms.RadioButton()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.CB_Region = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CB_DefaultMode = New System.Windows.Forms.ComboBox()
+        Me.CB_Enable = New System.Windows.Forms.CheckBox()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.CheckBox6 = New System.Windows.Forms.CheckBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CB_Theme = New System.Windows.Forms.ComboBox()
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox()
+        Me.CB_SkipIntroLogos = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.B_SaveChanges = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel5.SuspendLayout()
+        Me.Panel6.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(403, 334)
+        Me.Panel1.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(168, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(135, Byte), Integer))
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(65, Byte), Integer), CType(CType(80, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.Button2.Location = New System.Drawing.Point(330, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(41, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "£$€"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(100, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(377, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(22, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "?"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Panel2
+        '
+        Me.Panel2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel2.Controls.Add(Me.Panel5)
+        Me.Panel2.Controls.Add(Me.Panel4)
+        Me.Panel2.Controls.Add(Me.Panel3)
+        Me.Panel2.Controls.Add(Me.B_SaveChanges)
+        Me.Panel2.Location = New System.Drawing.Point(3, 37)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(397, 294)
+        Me.Panel2.TabIndex = 3
+        '
+        'Panel5
+        '
+        Me.Panel5.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel5.Controls.Add(Me.Panel6)
+        Me.Panel5.Controls.Add(Me.Label4)
+        Me.Panel5.Location = New System.Drawing.Point(3, 170)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(391, 92)
+        Me.Panel5.TabIndex = 19
+        '
+        'Panel6
+        '
+        Me.Panel6.Controls.Add(Me.TB_SpoofedUsername)
+        Me.Panel6.Controls.Add(Me.RB_UsernameSpoof)
+        Me.Panel6.Controls.Add(Me.RB_UsernameHide)
+        Me.Panel6.Controls.Add(Me.RB_UsernameShow)
+        Me.Panel6.Location = New System.Drawing.Point(3, 17)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(385, 71)
+        Me.Panel6.TabIndex = 13
+        '
+        'TB_SpoofedUsername
+        '
+        Me.TB_SpoofedUsername.Enabled = False
+        Me.TB_SpoofedUsername.Location = New System.Drawing.Point(204, 25)
+        Me.TB_SpoofedUsername.Name = "TB_SpoofedUsername"
+        Me.TB_SpoofedUsername.Size = New System.Drawing.Size(178, 20)
+        Me.TB_SpoofedUsername.TabIndex = 14
+        Me.TB_SpoofedUsername.Text = "Spoofed username..."
+        '
+        'RB_UsernameSpoof
+        '
+        Me.RB_UsernameSpoof.AutoSize = True
+        Me.RB_UsernameSpoof.Location = New System.Drawing.Point(3, 49)
+        Me.RB_UsernameSpoof.Name = "RB_UsernameSpoof"
+        Me.RB_UsernameSpoof.Size = New System.Drawing.Size(118, 17)
+        Me.RB_UsernameSpoof.TabIndex = 2
+        Me.RB_UsernameSpoof.Text = "Spoof my username"
+        Me.RB_UsernameSpoof.UseVisualStyleBackColor = True
+        '
+        'RB_UsernameHide
+        '
+        Me.RB_UsernameHide.AutoSize = True
+        Me.RB_UsernameHide.Location = New System.Drawing.Point(3, 26)
+        Me.RB_UsernameHide.Name = "RB_UsernameHide"
+        Me.RB_UsernameHide.Size = New System.Drawing.Size(112, 17)
+        Me.RB_UsernameHide.TabIndex = 1
+        Me.RB_UsernameHide.Text = "Hide my username"
+        Me.RB_UsernameHide.UseVisualStyleBackColor = True
+        '
+        'RB_UsernameShow
+        '
+        Me.RB_UsernameShow.AutoSize = True
+        Me.RB_UsernameShow.Checked = True
+        Me.RB_UsernameShow.Location = New System.Drawing.Point(3, 3)
+        Me.RB_UsernameShow.Name = "RB_UsernameShow"
+        Me.RB_UsernameShow.Size = New System.Drawing.Size(175, 17)
+        Me.RB_UsernameShow.TabIndex = 0
+        Me.RB_UsernameShow.TabStop = True
+        Me.RB_UsernameShow.Text = "Show my username in the menu"
+        Me.RB_UsernameShow.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Label4.Location = New System.Drawing.Point(3, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(66, 14)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Username"
+        '
+        'Panel4
+        '
+        Me.Panel4.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel4.Controls.Add(Me.CB_Region)
+        Me.Panel4.Controls.Add(Me.Label3)
+        Me.Panel4.Controls.Add(Me.CheckBox1)
+        Me.Panel4.Controls.Add(Me.CB_DefaultMode)
+        Me.Panel4.Controls.Add(Me.CB_Enable)
+        Me.Panel4.Location = New System.Drawing.Point(3, 3)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(391, 65)
+        Me.Panel4.TabIndex = 19
+        '
+        'CB_Region
+        '
+        Me.CB_Region.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.CB_Region.Font = New System.Drawing.Font("Tahoma", 8.25!)
+        Me.CB_Region.FormattingEnabled = True
+        Me.CB_Region.Items.AddRange(New Object() {"North America", "Europe", "Asia", "Oceania", "South and Central America", "South East Asia"})
+        Me.CB_Region.Location = New System.Drawing.Point(169, 40)
+        Me.CB_Region.Name = "CB_Region"
+        Me.CB_Region.Size = New System.Drawing.Size(152, 21)
+        Me.CB_Region.TabIndex = 15
+        Me.CB_Region.Tag = "Select default region..."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(3, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 14)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Mod"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Tahoma", 8.25!)
+        Me.CheckBox1.Location = New System.Drawing.Point(90, 17)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(96, 17)
+        Me.CheckBox1.TabIndex = 8
+        Me.CheckBox1.Text = "Run at startup"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CB_DefaultMode
+        '
+        Me.CB_DefaultMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.CB_DefaultMode.Font = New System.Drawing.Font("Tahoma", 8.25!)
+        Me.CB_DefaultMode.FormattingEnabled = True
+        Me.CB_DefaultMode.Items.AddRange(New Object() {"Solo", "Duo", "Squad", "SOLO FPP", "DUO FPP", "SQUAD FPP", "Custom match"})
+        Me.CB_DefaultMode.Location = New System.Drawing.Point(3, 40)
+        Me.CB_DefaultMode.Name = "CB_DefaultMode"
+        Me.CB_DefaultMode.Size = New System.Drawing.Size(160, 21)
+        Me.CB_DefaultMode.TabIndex = 14
+        Me.CB_DefaultMode.Tag = "Select default gamemode..."
+        '
+        'CB_Enable
+        '
+        Me.CB_Enable.AutoSize = True
+        Me.CB_Enable.Font = New System.Drawing.Font("Tahoma", 8.25!)
+        Me.CB_Enable.Location = New System.Drawing.Point(3, 17)
+        Me.CB_Enable.Name = "CB_Enable"
+        Me.CB_Enable.Size = New System.Drawing.Size(81, 17)
+        Me.CB_Enable.TabIndex = 10
+        Me.CB_Enable.Text = "Enable mod"
+        Me.CB_Enable.UseVisualStyleBackColor = True
+        '
+        'Panel3
+        '
+        Me.Panel3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel3.Controls.Add(Me.CheckBox6)
+        Me.Panel3.Controls.Add(Me.Label2)
+        Me.Panel3.Controls.Add(Me.CB_Theme)
+        Me.Panel3.Controls.Add(Me.CheckBox5)
+        Me.Panel3.Controls.Add(Me.CB_SkipIntroLogos)
+        Me.Panel3.Controls.Add(Me.CheckBox4)
+        Me.Panel3.Location = New System.Drawing.Point(3, 88)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(391, 62)
+        Me.Panel3.TabIndex = 18
+        '
+        'CheckBox6
+        '
+        Me.CheckBox6.AutoSize = True
+        Me.CheckBox6.Font = New System.Drawing.Font("Tahoma", 8.25!)
+        Me.CheckBox6.Location = New System.Drawing.Point(118, 44)
+        Me.CheckBox6.Name = "CheckBox6"
+        Me.CheckBox6.Size = New System.Drawing.Size(73, 17)
+        Me.CheckBox6.TabIndex = 17
+        Me.CheckBox6.Text = "Hide rules"
+        Me.CheckBox6.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Label2.Location = New System.Drawing.Point(3, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 14)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Theme"
+        '
+        'CB_Theme
+        '
+        Me.CB_Theme.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.CB_Theme.Font = New System.Drawing.Font("Tahoma", 8.25!)
+        Me.CB_Theme.FormattingEnabled = True
+        Me.CB_Theme.Items.AddRange(New Object() {"Minimal", "Blood", "MS paint"})
+        Me.CB_Theme.Location = New System.Drawing.Point(3, 17)
+        Me.CB_Theme.Name = "CB_Theme"
+        Me.CB_Theme.Size = New System.Drawing.Size(121, 21)
+        Me.CB_Theme.TabIndex = 2
+        Me.CB_Theme.Tag = "Select theme..."
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.Font = New System.Drawing.Font("Tahoma", 8.25!)
+        Me.CheckBox5.Location = New System.Drawing.Point(3, 44)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(109, 17)
+        Me.CheckBox5.TabIndex = 16
+        Me.CheckBox5.Text = "Hide Steam name"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'CB_SkipIntroLogos
+        '
+        Me.CB_SkipIntroLogos.AutoSize = True
+        Me.CB_SkipIntroLogos.Font = New System.Drawing.Font("Tahoma", 8.25!)
+        Me.CB_SkipIntroLogos.Location = New System.Drawing.Point(130, 19)
+        Me.CB_SkipIntroLogos.Name = "CB_SkipIntroLogos"
+        Me.CB_SkipIntroLogos.Size = New System.Drawing.Size(98, 17)
+        Me.CB_SkipIntroLogos.TabIndex = 9
+        Me.CB_SkipIntroLogos.Text = "Skip intro logos"
+        Me.CB_SkipIntroLogos.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Font = New System.Drawing.Font("Tahoma", 8.25!)
+        Me.CheckBox4.Location = New System.Drawing.Point(234, 19)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(124, 17)
+        Me.CheckBox4.TabIndex = 15
+        Me.CheckBox4.Text = "Hide version number"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'B_SaveChanges
+        '
+        Me.B_SaveChanges.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.B_SaveChanges.Font = New System.Drawing.Font("Tahoma", 8.25!)
+        Me.B_SaveChanges.Location = New System.Drawing.Point(150, 268)
+        Me.B_SaveChanges.Name = "B_SaveChanges"
+        Me.B_SaveChanges.Size = New System.Drawing.Size(97, 23)
+        Me.B_SaveChanges.TabIndex = 11
+        Me.B_SaveChanges.Text = "Save changes"
+        Me.B_SaveChanges.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 18.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(3, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(241, 29)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "BattlegroundsThemer"
+        '
+        'F_Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(427, 358)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "F_Main"
+        Me.Text = "F_Main"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel5.PerformLayout()
+        Me.Panel6.ResumeLayout(False)
+        Me.Panel6.PerformLayout()
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents TB_SpoofedUsername As TextBox
+    Friend WithEvents RB_UsernameSpoof As RadioButton
+    Friend WithEvents RB_UsernameHide As RadioButton
+    Friend WithEvents RB_UsernameShow As RadioButton
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents CB_Region As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CB_DefaultMode As ComboBox
+    Friend WithEvents CB_Enable As CheckBox
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents CheckBox6 As CheckBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CB_Theme As ComboBox
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents CB_SkipIntroLogos As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents B_SaveChanges As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ BattlegroundsThemer/F_Main.vb
@@ -0,0 +1,3 @@
+﻿Public Class F_Main
+
+End Class
