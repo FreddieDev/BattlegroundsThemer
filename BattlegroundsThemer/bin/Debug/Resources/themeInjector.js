c523491268420e9966a52b437d3ab40c8525d7cf
FreddieDev <4047565+FreddieDev@users.noreply.github.com>
2017-10-23 13:29:34 +0100

    Recieve invites checkmark now in sync with game, version number alignment fix, online/active friend counts now functions. Next up - fix alignment of logo, userstats and sync friends menu and play buttons!

--- BattlegroundsThemer/bin/Debug/Resources/themeInjector.js
+++ BattlegroundsThemer/bin/Debug/Resources/themeInjector.js
@@ -2,6 +2,7 @@
 var container_play; // The play menu/buttons
 var container_userInfo; // Holds username and BP
 var container_header; // Holds the tabbar
+var container_recieveInvitesCheckbox; // Holds the recieving invites checkbox and styles the state
 
 // Declare UI element variables
 var menu_playButton;
@@ -16,6 +17,7 @@ var menu_rewardsTab;
 var menu_statisticsTab;
 var menu_leaderboardsTab;
 var menu_notification;
+var menu_inviteFriendButton;
 
 // Declare custom UI element variables
 var customMenu_playButton;
@@ -29,6 +31,9 @@ var customMenu_rewardsTab;
 var customMenu_statisticsTab;
 var customMenu_leaderboardsTab;
 var customMenu_notification;
+var customMenu_friendsPlayingCount;
+var customMenu_friendsOnlineCount;
+var customMenu_recieveInvitesCheckbox;
 
 
 function loadElementsIntoVars() {
@@ -36,6 +41,7 @@ function loadElementsIntoVars() {
 	container_play = 		document.getElementsByTagName("select-game")[0];
 	container_userInfo =	document.getElementsByClassName("bro-userinfo")[0];
 	container_header = 		document.querySelector(".bro-gnb").getElementsByTagName("ul")[0];
+	container_recieveInvitesCheckbox = 	document.getElementsByClassName("inviteFriend")[0];
 	
 	
 	// Assign UI element variables
@@ -51,23 +57,32 @@ function loadElementsIntoVars() {
 	menu_statisticsTab = 	document.querySelector("button[routerlinkactive]:not([routerlink])");
 	menu_leaderboardsTab =	document.querySelector("button[routerlink='game/leaderboard']");
 	
-	//menu_notification =
+	//menu_notification removed
+	
+	menu_inviteFriendButton =	document.getElementsByClassName("invite-check")[0];
 	
 	
 	
 	// Assign custom UI element variables
-	customMenu_playButton = 		document.getElementById("battlegroundsThemer_play");
-	customMenu_username = 			document.getElementById("battlegroundsThemer_username");
-	customMenu_battlepoints = 		document.getElementById("battlegroundsThemer_battlepoints");
-	customMenu_version = 			document.getElementById("battlegroundsThemer_version");
-	customMenu_logo = 				document.getElementById("battlegroundsThemer_logo");
-	customMenu_homeTab = 			document.getElementById("battlegroundsThemer_home");
-	customMenu_characterTab = 		document.getElementById("battlegroundsThemer_character");
-	customMenu_rewardsTab = 		document.getElementById("battlegroundsThemer_rewards");
-	customMenu_statisticsTab =		document.getElementById("battlegroundsThemer_stats");
-	customMenu_leaderboardsTab =	document.getElementById("battlegroundsThemer_leaderboard");
-	customMenu_notification =		document.getElementById("battlegroundsThemer_notification");
+	customMenu_playButton = 			document.getElementById("battlegroundsThemer_play");
+	customMenu_username = 				document.getElementById("battlegroundsThemer_username");
+	customMenu_battlepoints = 			document.getElementById("battlegroundsThemer_battlepoints");
+	customMenu_version = 				document.getElementById("battlegroundsThemer_version");
+	customMenu_logo = 					document.getElementById("battlegroundsThemer_logo");
+	customMenu_homeTab = 				document.getElementById("battlegroundsThemer_home");
+	customMenu_characterTab = 			document.getElementById("battlegroundsThemer_character");
+	customMenu_rewardsTab = 			document.getElementById("battlegroundsThemer_rewards");
+	customMenu_statisticsTab =			document.getElementById("battlegroundsThemer_stats");
+	customMenu_leaderboardsTab =		document.getElementById("battlegroundsThemer_leaderboard");
+	customMenu_notification =			document.getElementById("battlegroundsThemer_notification");
+	
+	customMenu_friendsPlayingCount = 	document.getElementById("battlegroundsThemer_friendsPlayingCount");
+	customMenu_friendsOnlineCount = 	document.getElementById("battlegroundsThemer_friendsOnlineCount");
+	customMenu_recieveInvitesCheckbox = document.getElementById("battlegroundsThemer_recieveInvites");
 }
+
+
+
 
 function manageReplacedUI() {
 	loadElementsIntoVars();
@@ -79,6 +94,7 @@ function manageReplacedUI() {
 	if (customMenu_battlepoints || customMenu_username) container_userInfo.style.opacity = "0";
 	if (customMenu_version) menu_version.style.display = "none";
 	if (customMenu_homeTab) container_header.style.display = "none";
+	if (customMenu_recieveInvitesCheckbox) container_recieveInvitesCheckbox.style.display = "none";
 
 	// Remove default menu logo:
 	if (customMenu_logo && !hasHiddenMenuLogo) {
@@ -88,6 +104,10 @@ function manageReplacedUI() {
 		hasHiddenMenuLogo = true;
 	}
 }
+
+
+
+
 
 function initThemeInjection() {
 	// --------------------------------------------
@@ -99,6 +119,7 @@ function initThemeInjection() {
 	link.href = "data:text/css," + "BT_CSSHERE";
 	document.getElementsByTagName("HEAD")[0].appendChild(link);
 	
+	
 	// --------------------------------------------
 	// Inject theme HTML into top of body
 	// --------------------------------------------
@@ -109,18 +130,8 @@ function initThemeInjection() {
 	themeHTML.style.left = "0";
 	themeHTML.style.top = "0";
 	themeHTML.innerHTML = "BT_HTMLHERE";
-	// (U*HASD)IUY*HASD)(*&YUHASD)(&*HAWD)S*UIYHAOIWU*YDHA)*S&HD)*(A&YSHGD)*&ASHGD*&)g
-	// (U*HASD)IUY*HASD)(*&YUHASD)(&*HAWD)S*UIYHAOIWU*YDHA)*S&HD)*(A&YSHGD)*&ASHGD*&)g
-	// (U*HASD)IUY*HASD)(*&YUHASD)(&*HAWD)S*UIYHAOIWU*YDHA)*S&HD)*(A&YSHGD)*&ASHGD*&)g
-	// (U*HASD)IUY*HASD)(*&YUHASD)(&*HAWD)S*UIYHAOIWU*YDHA)*S&HD)*(A&YSHGD)*&ASHGD*&)g
-	// CONTINUE WORK FROM HERE:
-	// Note: If the code is inserted using body.appendChild the menu can't be clicked
-	//document.body.appendChild(themeHTML);
-	//var documentHeader = document.body.getElementsByTagName("header")[0];
-	//document.body.insertBefore(themeHTML, documentHeader); // document.body.firstChild
 	var elemToInjectInto = document.querySelector("nav");
 	elemToInjectInto.appendChild(themeHTML);
-	
 	elemToInjectInto.parentElement.parentElement.style.position = "absolute";
 	elemToInjectInto.parentElement.parentElement.style.height = "100%";
 	elemToInjectInto.parentElement.parentElement.style.width = "100%";
@@ -136,26 +147,44 @@ function initThemeInjection() {
 	elemToInjectInto.parentElement.parentElement.style.background = "initial";
 	elemToInjectInto.parentElement.parentElement.style.pointerEvents = "none";
 
+	
 	// --------------------------------------------
 	// Hide default page elements
 	// --------------------------------------------
 	document.addEventListener('click', manageReplacedUI);
 	manageReplacedUI();
 	
+	
 	// --------------------------------------------
 	// Attach event handlers to rethemed elements
 	// --------------------------------------------
 	customMenu_playButton.onclick = function(event) {
-		if (event.target == customMenu_playButton) {
-			menu_playButton.click();
-		}
+		if (event.target == customMenu_playButton) menu_playButton.click();
 	}
 	
-	// Click version number to reload page for debugging
+	customMenu_recieveInvitesCheckbox.onchange = function(event) {
+		// Disable future toggles until the checkbox is back in sync
+		customMenu_recieveInvitesCheckbox.disabled = true;
+		
+		// Click the original recieve button
+		menu_inviteFriendButton.click();
+		
+		// Wait 3 seconds to renable
+		setTimeout(function () {
+			customMenu_recieveInvitesCheckbox.disabled = false;
+			
+			// Update recieving invites checkbox
+			customMenu_recieveInvitesCheckbox.checked = container_recieveInvitesCheckbox.className.includes("checked");
+		}, 3000);
+	}
+	
+	
+	// Click version number to show updates and news
 	customMenu_version.addEventListener('click', function (event) {
 		engine.trigger('OpenExternalBrowser', 'http://steamcommunity.com/app/578080/announcements/');
 	});
 	
+	// Click game logo to reload page
 	customMenu_logo.addEventListener('click', function (event) {
 		location.reload();
 	});
@@ -165,6 +194,8 @@ function initThemeInjection() {
 	customMenu_rewardsTab.onclick = function(event) { menu_rewardsTab.click(); }
 	customMenu_statisticsTab.onclick = function(event) { menu_statisticsTab.click(); }
 	customMenu_leaderboardsTab.onclick = function(event) { menu_leaderboardsTab.click(); }
+	
+	
 	
 	// --------------------------------------------
 	// Update custom UI element static content
@@ -184,6 +215,7 @@ function initThemeInjection() {
 	// --------------------------------------------
 	var richMemeCounter = 100000;
 	var syncBattlepoints = setInterval(function() {
+		// Keep battlepoints in sync
 		if ("BT_IAMSORICH" != "BT_" + "IAMSORICH") {
 			if (richMemeCounter == 100000) {
 				setInterval(function() {
@@ -192,10 +224,10 @@ function initThemeInjection() {
 				}, 10);
 			}
 		} else {
-			// Keep battlepoints in sync
 			var menu_battlepoints = document.getElementsByClassName("userpoint")[0].getElementsByTagName("em")[0];
 			customMenu_battlepoints.innerText = menu_battlepoints.innerText;
 		}
+		
 		
 		// Keep notifications in sync
 		menu_notification = document.getElementsByClassName("notice-msg")[0];
@@ -207,8 +239,16 @@ function initThemeInjection() {
 		}
 		
 		
-	}, 200);
+		// Update friends online/playing counts
+		var steamStatsContainer = document.getElementsByClassName("bro-joinFriend")[0];
+		if (customMenu_friendsPlayingCount) customMenu_friendsPlayingCount.innerText = steamStatsContainer.getElementsByTagName("em")[0].innerText;
+		if (customMenu_friendsOnlineCount) customMenu_friendsOnlineCount.innerText = steamStatsContainer.getElementsByTagName("strong")[0].innerText;
+		
+	}, 500);
 }
+
+
+
 
 
 var waitForFullyLoaded = setInterval(function() {
